6a2010e6c19106df5337fe035b6bf8508d863774
Harshit Gusain <144123326+harshitgusain@users.noreply.github.com>
2026-01-07 17:12:44 +0530

    Add files via upload

---
+++ assignment 1 task 1.py
@@ -0,0 +1,9 @@
+# Take two numbers from the user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Perform operations
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
